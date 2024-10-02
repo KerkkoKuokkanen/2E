c232ecd46e134d4be77cee47ff4a5a5815568945
kkuokkan <kerkko.kuokkanen@gmail.com>
2024-10-02 16:58:59 +0300

    sprite rendering system progress

--- srcs/GL_Stuff/EBO.d
+++
@@ -1,3 +0,0 @@
-srcs/GL_Stuff/EBO.o: srcs/GL_Stuff/EBO.cpp hdr/GL_Stuff/EBO.h
-
-hdr/GL_Stuff/EBO.h:
--- srcs/GL_Stuff/ShaderClass.d
+++
@@ -1,4 +0,0 @@
-srcs/GL_Stuff/ShaderClass.o: srcs/GL_Stuff/ShaderClass.cpp \
-  hdr/GL_Stuff/ShaderClass.h
-
-hdr/GL_Stuff/ShaderClass.h:
