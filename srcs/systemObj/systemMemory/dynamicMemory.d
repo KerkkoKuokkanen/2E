fae84d4ee58bfd273f7e52d9106a77f68a7e423b
kkuokkan <kerkko.kuokkanen@gmail.com>
2024-11-17 15:25:09 +0200

    memory pool

--- srcs/systemObj/systemMemory/dynamicMemory.d
+++
@@ -1,8 +0,0 @@
-srcs/systemObj/systemMemory/dynamicMemory.o: \
-  srcs/systemObj/systemMemory/dynamicMemory.cpp \
-  hdr/systemObj/systemMemory/dynamicMemory.h \
-  hdr/systemObj/systemMemory/memoryPool.h
-
-hdr/systemObj/systemMemory/dynamicMemory.h:
-
-hdr/systemObj/systemMemory/memoryPool.h:
