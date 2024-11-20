88d7f5f51ce4184a6df07d845be732aefffe5a24
kkuokkan <kerkko.kuokkanen@gmail.com>
2024-11-20 15:16:02 +0200

    mid

--- srcs/systemObj/sysEnv/sysEnv.d
+++
@@ -1,6 +0,0 @@
-srcs/systemObj/sysEnv/sysEnv.o: srcs/systemObj/sysEnv/sysEnv.cpp \
-  hdr/systemObj/sysEnv/sysEnv.h hdr/systemObj/systemObj.h
-
-hdr/systemObj/sysEnv/sysEnv.h:
-
-hdr/systemObj/systemObj.h:
--- srcs/systemObj/sysEnv/sysEnvSave.d
+++
@@ -1,6 +0,0 @@
-srcs/systemObj/sysEnv/sysEnvSave.o: srcs/systemObj/sysEnv/sysEnvSave.cpp \
-  hdr/systemObj/sysEnv/sysEnv.h hdr/systemObj/systemObj.h
-
-hdr/systemObj/sysEnv/sysEnv.h:
-
-hdr/systemObj/systemObj.h:
