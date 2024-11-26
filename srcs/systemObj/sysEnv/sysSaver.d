e4412f4ae7e4e1e02b6078ff664be5fb99c046bf
kkuokkan <kerkko.kuokkanen@gmail.com>
2024-11-26 16:16:37 +0200

    progress, small but some

--- srcs/systemObj/sysEnv/sysSaver.d
+++
@@ -1,9 +0,0 @@
-srcs/systemObj/sysEnv/sysSaver.o: srcs/systemObj/sysEnv/sysSaver.cpp \
-  hdr/systemObj/sysEnv/sysSaver.h hdr/systemObj/systemObj.h \
-  hdr/Tools/common/commonTools.h
-
-hdr/systemObj/sysEnv/sysSaver.h:
-
-hdr/systemObj/systemObj.h:
-
-hdr/Tools/common/commonTools.h:
